4b604cff20ce73ef031aa2790109f3b6a516cadd
Autumn Pan <pan662607@gmail.com>
2026-01-22 09:25:23 -0500

    [refactor]: move decider headers into decider.h

---
+++ include/decider.h
@@ -0,0 +1,33 @@
+#ifndef DECIDER_H
+#define DECIDER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "tm.h"
+
+#ifndef DECIDER_PARAMS
+  #define CT_DEPTH 32
+  #define HPR_TOLERANCE 4
+#endif
+
+// Reduced Configuration Tree (RCT)
+typedef struct halting_signature {
+  struct halting_signature **children;
+  uint64_t *tape;
+  size_t head_index;
+  char state;
+} HaltingSignature;
+
+// Extended Backwards Reasoning Decider
+typedef struct {
+  HaltingSignature *halting_signature;
+  HaltingSignature *current_node;
+  HaltingSignature **unexplored_leaves;
+
+  Instruction_t rules[STATES][SYMBOLS];
+  size_t num_unsolved;
+} EBRDecider;
+
+
+#endif
